5e49e2f96e41653c4b63d7146eca2c887c8dd600
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-22 08:50:24 -0700

    Add all class method and save method to Volunteer.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -9,7 +9,23 @@ class Volunteer
     @project_id = attributes[:project_id]
   end
 
+  def save
+    results = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = results.first["id"].to_i
+  end
+
   def ==(other_volunteer)
     @name == other_volunteer.name
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM volunteers;")
+    results.map do |result|
+      Volunteer.new({
+        id: result["id"].to_i,
+        name: result["name"],
+        project_id: result["project_id"].to_i
+      })
+    end
   end
 end
