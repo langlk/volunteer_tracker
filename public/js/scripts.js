fa54d88af6cfd7331f819df4db154595865c56bb
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-22 14:58:47 -0700

    Add JS to prevent empty volunteer form input.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
-  $(".project-info").click(function(event) {
+  $(".project-submit").click(function(event) {
     var title = $("#title").val()
     if (title.length == 0) {
       event.preventDefault()
       $(".alert").show()
       $(".title").addClass("has-error")
+    }
+  });
+
+  $(".volunteer-submit").click(function(event) {
+    var name = $("#name").val()
+    if (name.length == 0) {
+      event.preventDefault()
+      $(".volunteer-error").show()
+      $(".name").addClass("has-error")
     }
   });
 });
