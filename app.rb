55b22a36de2a5bf70c5a5c55ef4a7ac73e811f49
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-22 09:09:14 -0700

    Create initial Sinatra files.

---
+++ app.rb
@@ -0,0 +1,13 @@
+require "sinatra"
+require "sinatra/reloader"
+also_reload "lib/**/*.rb"
+require "pg"
+require "./lib/project"
+require "./lib/volunteer"
+require "pry"
+
+DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+
+get('/') do
+  erb(:index)
+end
