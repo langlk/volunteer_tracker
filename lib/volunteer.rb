08597b6dfec15167948229d91b5db9882d630a29
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-22 08:46:05 -0700

    Add initialize and attr_reader methods to Volunteer.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
 
 class Volunteer
+  attr_reader :id, :name, :project_id
+
+  def initialize(attributes)
+    @id = attributes[:id] || nil
+    @name = attributes[:name]
+    @project_id = attributes[:project_id]
+  end
 end
